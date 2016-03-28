d1822bd935267076fd41d07d2c5c2bfeca92164f
JM Versteeg <janmerijnversteeg@gmail.com>
2016-03-28 21:21:09 +0200

    Fix removeResults method

--- test/index.js
+++ test/index.js
@@ -215,4 +215,25 @@ describe('Cache', () => {
                 });
         });
     });
+
+    describe('.removeResults', () => {
+        it('should remove the given results', () => {
+            let results;
+            return Promise.all([
+                    cache.doCached(sinon.spy(), defaultOptions),
+                    cache.doCached(sinon.spy(), _.assign({}, defaultOptions, {'action': 'op 2'})),
+                    cache.doCached(sinon.spy(), _.assign({}, defaultOptions, {'action': 'op 3'}))
+                ])
+                .then(r => {
+                    results = r;
+                    return cache.getAllDocs();
+                })
+                .then(docs => {
+                    docs.should.have.length(3);
+                    return cache.removeResults([results[0], results[2]]);
+                })
+                .then(() => cache.getAllDocs())
+                .then(docs => docs.should.have.length(1).and.have.deep.property('0._id', results[1]._id));
+        });
+    });
 });
