ac46f6ffd828eeb40611754771832f00393374ab
JM Versteeg <janmerijnversteeg@gmail.com>
2016-03-28 14:34:50 +0200

    Add support for "no input" operatinos

--- test/index.js
+++ test/index.js
@@ -43,67 +43,99 @@ describe('Cache', ()=> {
     afterEach(()=> {
         return resetWorkspace();
     });
+
     describe('.doCached', ()=> {
-        it('should not run the callable a second time if the input files stayed the same', () => {
-            let run = sinon.spy();
-            return cache.doCached(run, defaultOptions)
-                .then(() => uncopy())
-                .then(() => cache.doCached(run, defaultOptions))
-                .then(() => run.should.have.been.calledOnce);
-        });
-        it('should run the callable twice if the input files were modified', () => {
-            let run = sinon.spy(copy);
-            return cache.doCached(run, defaultOptions)
-                .then(() => modify())
-                .then(() => cache.doCached(run, defaultOptions))
-                .then(() => run.should.have.been.calledTwice);
-        });
-        it('should run the callable twice if the output argument is different', () => {
-            let run = sinon.spy(copy);
-            return cache.doCached(run, defaultOptions)
-                .then(() => cache.doCached(run, _.assign(defaultOptions, {
-                    output: ['test/sample/build/*.txt']
-                })))
-                .then(() => run.should.have.been.calledTwice);
-        });
-        it('should run the callable twice if the cached version has expired', () => {
-            let run     = sinon.spy(copy);
-            let options = _.assign({}, defaultOptions, {'ttl': -1});
-            return cache.doCached(run, options)
-                .then(() => uncopy())
-                .then(() => cache.doCached(run, options))
-                .then(() => run.should.have.been.calledTwice);
-        });
-        it('should restore the operation from cache the second time if the input files stayed the same', () => {
-            return cache.doCached(copy, defaultOptions)
-                .then(() => uncopy())
-                .then(() => cache.doCached(copy, defaultOptions))
-                .then(() => pify(fs.readFile)('test/sample/build/foo.txt', {encoding: 'utf8'}))
-                .then((data) => {
-                    data.should.equal('bar');
+
+        _.forEach({
+
+            'input and output with wildcard': defaultOptions,
+
+            'input and output as folder names': {
+                action: 'op',
+                input:  ['test/sample/assets'],
+                output: ['test/sample/build'],
+                ttl:    60000
+            },
+
+
+            'no input given': {
+                action: 'op',
+                output: ['test/sample/build'],
+                ttl:    60000
+            }
+
+        }, (defaultOptions, description) => {
+
+            describe(description, () => {
+
+                it('should not run the callable a second time if the input files stayed the same', () => {
+                    let run = sinon.spy();
+                    return cache.doCached(run, defaultOptions)
+                        .then(() => uncopy())
+                        .then(() => cache.doCached(run, defaultOptions))
+                        .then(() => run.should.have.been.calledOnce);
                 });
-        });
-        it('should work with the compress option set to true', () => {
-            let options = _.assign({}, defaultOptions, {'compress': true});
-            return cache.doCached(copy, options)
-                .then(() => uncopy())
-                .then(() => cache.doCached(copy, options))
-                .then(() => pify(fs.readFile)('test/sample/build/foo.txt', {encoding: 'utf8'}))
-                .then((data) => {
-                    data.should.equal('bar');
+
+                if (defaultOptions.input)
+                    it('should run the callable twice if the input files were modified', () => {
+                        let run = sinon.spy(copy);
+                        return cache.doCached(run, defaultOptions)
+                            .then(() => modify())
+                            .then(() => cache.doCached(run, defaultOptions))
+                            .then(() => run.should.have.been.calledTwice);
+                    });
+
+                it('should run the callable twice if the output argument is different', () => {
+                    let run = sinon.spy(copy);
+                    return cache.doCached(run, defaultOptions)
+                        .then(() => cache.doCached(run, _.assign(defaultOptions, {
+                            output: ['test/sample/build/*.txt']
+                        })))
+                        .then(() => run.should.have.been.calledTwice);
                 });
-        });
-        it('should generate a file at the location corresponding to the cached result', () => {
-            let run = sinon.spy(copy);
-            return cache.doCached(run, defaultOptions)
-                .then(cachedResult => pify(fs.access)(cache.getStorageLocation(cachedResult)));
-        });
-        it('should fallback gracefully if the cached result has no corresponding file', () => {
-            let run = sinon.spy(copy);
-            return cache.doCached(run, defaultOptions)
-                .then(cachedResult => del(cache.getStorageLocation(cachedResult)))
-                .then(() => cache.doCached(run, defaultOptions))
-                .then(() => run.should.have.been.calledTwice);
+
+                it('should run the callable twice if the cached version has expired', () => {
+                    let run     = sinon.spy(copy);
+                    let options = _.assign({}, defaultOptions, {'ttl': -1});
+                    return cache.doCached(run, options)
+                        .then(() => uncopy())
+                        .then(() => cache.doCached(run, options))
+                        .then(() => run.should.have.been.calledTwice);
+                });
+
+                it('should restore the operation from cache the second time if the input files stayed the same', () => {
+                    return cache.doCached(copy, defaultOptions)
+                        .then(() => uncopy())
+                        .then(() => cache.doCached(copy, defaultOptions))
+                        .then(() => pify(fs.readFile)('test/sample/build/foo.txt', {encoding: 'utf8'}))
+                        .then((data) => {
+                            data.should.equal('bar');
+                        });
+                });
+                it('should work with the compress option set to true', () => {
+                    let options = _.assign({}, defaultOptions, {'compress': true});
+                    return cache.doCached(copy, options)
+                        .then(() => uncopy())
+                        .then(() => cache.doCached(copy, options))
+                        .then(() => pify(fs.readFile)('test/sample/build/foo.txt', {encoding: 'utf8'}))
+                        .then((data) => {
+                            data.should.equal('bar');
+                        });
+                });
+                it('should generate a file at the location corresponding to the cached result', () => {
+                    let run = sinon.spy(copy);
+                    return cache.doCached(run, defaultOptions)
+                        .then(cachedResult => pify(fs.access)(cache.getStorageLocation(cachedResult)));
+                });
+                it('should fallback gracefully if the cached result has no corresponding file', () => {
+                    let run = sinon.spy(copy);
+                    return cache.doCached(run, defaultOptions)
+                        .then(cachedResult => del(cache.getStorageLocation(cachedResult)))
+                        .then(() => cache.doCached(run, defaultOptions))
+                        .then(() => run.should.have.been.calledTwice);
+                });
+
+            });
         });
     });
     describe('.purgeExpired', () => {
