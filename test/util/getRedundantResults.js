f06ef41c6d901d37f1f9995bab210d4a6e8c8454
JM Versteeg <janmerijnversteeg@gmail.com>
2016-03-28 20:55:44 +0200

    Add tests for getRedundantResults

---
+++ test/util/getRedundantResults.js
@@ -0,0 +1,47 @@
+'use strict';
+
+require('./../support/bootstrap');
+
+const sinon               = require('sinon');
+const CachedResult        = require('./../../lib/CachedResult');
+const getRedundantResults = require('./../../lib/util/getRedundantResults');
+
+const makeMockResult = fileSize => {
+    return {
+        fileSize
+    };
+};
+
+describe('getRedundantResults', () => {
+
+    let r1 = makeMockResult(501);
+    let r2 = makeMockResult(502);
+    let r3 = makeMockResult(503);
+    let r4 = makeMockResult(504);
+
+    it('should determine what results to remove in order to prevent exceeding the maximum cache size', () => {
+
+        let getScoreStub = sinon.stub();
+
+        getScoreStub.withArgs(r1).returns(1);
+        getScoreStub.withArgs(r2).returns(2);
+        getScoreStub.withArgs(r3).returns(3);
+        getScoreStub.withArgs(r4).returns(0);
+
+        getRedundantResults.getScore = getScoreStub;
+
+        var redundantResults = getRedundantResults([
+            r1, r2, r3, r4
+        ], 1000, 2000);
+
+        redundantResults.should.have.length(2)
+            .and.include(r1)
+            .and.include(r4);
+    });
+    
+    it('should return an empty array if there is no need to remove any results', () => {
+        
+        
+        
+    });
+});
