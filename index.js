388131ace3c086dc16ecd16e482a2eed353f1082
JM Versteeg <janmerijnversteeg@gmail.com>
2017-07-25 18:45:51 +0200

    add dedicated entrypoint

---
+++ index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const Cache = require('./lib/Cache');
+
+const CacheFlow = require('./lib/CacheFlow');
+const Intent    = require('./lib/Intent');
+
+const StoringResult     = require('./lib/StoringResult');
+const SavedToCache      = require('./lib/SavedToCache');
+const RestoredFromCache = require('./lib/RestoredFromCache');
+
+module.exports = Cache;
+
+module.exports.CacheFlow = CacheFlow;
+module.exports.Intent    = Intent;
+
+module.exports.StoringResult     = StoringResult;
+module.exports.SavedToCache      = SavedToCache;
+module.exports.RestoredFromCache = RestoredFromCache;
